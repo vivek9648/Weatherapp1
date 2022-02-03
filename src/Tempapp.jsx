3fd04d4dcc5fdc440959077dfc4fcc36c9338346
vivek <svivekkumar430@gmail.com>
2022-02-03 18:03:06 +0530

    Initial commit

---
+++ src/Tempapp.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import "./css/style.css"
+const Tempapp=()=>{
+    <div className="box">
+        <div className="inputData">
+            <input type="search" className='inputField' />
+        </div>
+    </div>
+
+}
+export default Tempapp;
